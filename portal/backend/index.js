6d3111ae60a749228b15743b6319b6c42b7bb32d
parseccentric <8165314+parseccentric@users.noreply.github.com>
2025-03-13 19:57:01 -0500

    add debug handlers for database connection

--- portal/backend/index.js
+++ portal/backend/index.js
@@ -18,6 +18,15 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
+pool.on("connection", connection => console.debug(`connection established: ${connection.threadId}`));
+pool.on("acquire", connection => console.debug(`acquired: ${connection.threadId}`));
+pool.on("release", connection => console.debug(`released: ${connection.threadId}`));
+pool.on("enqueue", () => console.log("enqueued"));
+
+pool.getConnection((error, connection) => {
+  if (error) throw error;
+})
+
 const app = express();
 const port = config.web.port;
 
@@ -61,7 +70,7 @@ app.use("/data", dataController);
 app.use("/public", publicController);
 app.use("/turn", turnController);
 
-app.get('/', (req, res) => res.send('Hello Komodo!'));
+app.get('/', (req, res) => res.send('Hello Komodo!' + pool));
 
 app.get('/s3_signed/:name', (req, res) => {
   const name = req.params.name;
