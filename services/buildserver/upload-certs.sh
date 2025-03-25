d1bfe1039fc359d621c0995d8619e551fc312cad
parseccentric <8165314+parseccentric@users.noreply.github.com>
2025-03-25 18:04:09 -0500

    whoops

--- services/buildserver/upload-certs.sh
+++ services/buildserver/upload-certs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-scp -OP 443 ./immersityxr-local.crt root@localhost:/etc/ssl/certs/immersityxr-local.crt
-scp -OP 443 ./immersityxr-local.key root@localhost:/etc/ssl/private/immersityxr-local.key
+IMMERSITY_BUILD_HOST=localhost
+scp -OP 443 ./immersityxr-local.crt root@$IMMERSITY_BUILD_HOST:/etc/ssl/certs/immersityxr-local.crt
+scp -OP 443 ./immersityxr-local.key root@$IMMERSITY_BUILD_HOST:/etc/ssl/private/immersityxr-local.key
 
-ssh -p 443 root@localhost 'nginx -s reload'
+ssh -p 443 root@$IMMERSITY_BUILD_HOST 'nginx -s reload'
