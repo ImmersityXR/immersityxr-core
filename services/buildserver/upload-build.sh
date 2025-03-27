a1c0e0bba61523ce570be4454a8e8565c1b40d97
parseccentric <8165314+parseccentric@users.noreply.github.com>
2025-03-27 02:26:37 -0500

    upload a directory recursively using for loops with curl

--- services/buildserver/upload-build.sh
+++ services/buildserver/upload-build.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
-IMMERSITY_BUILD_HOST=immersitybuild-test-cwfpekgjcfbkfbat.northcentralus-01.azurewebsites.net
-curl root@$IMMERSITY_BUILD_HOST /home/public/
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./upload-build.sh <directory> <host>" >&2
+    exit 2
+fi
+
+host=$2
+
+cd $1
+cur_dir=$(basename $PWD)
+cd ..
+
+# to verify which directories are being uploaded, uncomment this line:
+# find v0.5.7 -type d -exec echo "{}/" \;
+
+# the trailing slash is necessary for creating directories
+# find $cur_dir -type d -exec curl --ftp-create-dirs -T {} "$host/{}/" \;
+for dir in $(find $cur_dir -type d)
+do
+    curl --ftp-create-dirs -T $dir $host/$dir/;
+done
+
+# to verify which files are being uploaded, uncomment this line:
+# find v0.5.7 -type f
+# find $cur_dir -type f -exec curl -T {} "$host/{}" \;
+for file in $(find $cur_dir -type f)
+do
+    curl -T $file $host/$file;
+done
