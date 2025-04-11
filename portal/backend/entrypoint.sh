b2b97a2e38398c47e4296c1db7bdb53856b8a302
parseccentric <8165314+parseccentric@users.noreply.github.com>
2025-04-10 22:51:59 -0500

    finally get the database to work

--- portal/backend/entrypoint.sh
+++ portal/backend/entrypoint.sh
@@ -4,13 +4,16 @@ openrc default
 rc-status
 
 /etc/init.d/mariadb setup
-/usr/bin/mariadbd-safe --datadir='/home/mysql' --nowatch
+/usr/bin/mariadbd-safe --datadir='/home/mysql' --nowatch --socket='/run/mysqld/mysqld.sock'
 
+sleep 60
 # rc-service mariadb start
 # rc-update add mariadb default
 
 cd /komodo/portal/backend/db/scripts
 mariadb-admin --verbose create $MYSQL_DATABASE
+echo "CREATE USER IF NOT EXISTS $MYSQL_USER@localhost IDENTIFIED BY '$MYSQL_PASSWORD';" | mariadb
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@localhost;" | mariadb
 mariadb --database=$MYSQL_DATABASE < 01_CreateInitialTables.sql
 mariadb --database=$MYSQL_DATABASE < 02_InsertInitialData.sql
 mariadb --database=$MYSQL_DATABASE < 03_InsertDummyAssets.sql
